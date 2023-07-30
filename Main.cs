fc6f454df93114338d7763e6389ab1cf203b2b2f
CVEzeroday <112877063+CVEzeroday@users.noreply.github.com>
2023-07-31 01:18:27 +0900

    initial commit

---
+++ Main.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace ADOFAI_CMO
+{
+  public class Main
+  {
+    public static UnityModManager.ModEntry.ModLogger Logger;
+    public static Harmony harmony;
+    public static bool IsEnabled = false;
+
+    public static void Setup(UnityModManager.ModEntry modEntry)
+    {
+      Logger = modEntry.Logger;
+      modEntry.OnToggle = OnToggle;
+    }
+
+    public static bool OnToggle(UnityModManager.ModEntry modEntry, bool val)
+    {
+      IsEnabled = val;
+
+      if (val)
+      {
+        harmony = new Harmony(modEntry.Info.Id);
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+      }
+      else harmony.UnpatchAll(modEntry.Info.Id);
+
+      return true;
+    }
+
+  }
+}
---
+++ Patch.cs
@@ -0,0 +1,48 @@
+﻿using ADOFAI;
+using HarmonyLib;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOFAI_CMO
+{
+  [HarmonyPatch(typeof(scrExtImgHolder), "LoadTexture")]
+  internal class Patch
+  {
+    public static bool Prefix(ref Texture2D __result, string filePath, out LoadResult status, int maxSideSize = -1)
+    {
+      status = LoadResult.MissingFile;
+      Texture2D texture2D;
+      if (GCS.internalLevelName != null)
+      {
+        return true;
+      }
+      else
+      {
+        if (!RDFile.Exists(filePath))
+        {
+          __result = (Texture2D)null;
+          return false;
+        }
+        byte[] data = RDFile.ReadAllBytes(filePath, out status);
+        texture2D = new Texture2D(2, 2, TextureFormat.RGBA32, false);
+        if (!texture2D.LoadImage(data))
+        {
+          __result = (Texture2D)null;
+          return false;
+        }
+        if (maxSideSize != -1)
+          scrExtImgHolder.ShrinkImage(texture2D, maxSideSize);
+        texture2D.name = filePath;
+      }
+      texture2D.wrapMode = TextureWrapMode.Repeat;
+      __result = texture2D;
+      return false;
+    }
+
+  }
+}
